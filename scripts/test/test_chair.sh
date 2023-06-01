168fd98949ae09d0b5f5c958cf9c56f7f6094ab8
Rundong Luo <redfairy@svl17.stanford.edu>
2023-06-01 02:54:08 -0700

    init

---
+++ scripts/test/test_chair.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DATAROOT=${1:-'/viscam/projects/uorf-extension/datasets/room_chair/test'}
+PORT=${2:-12783}
+python test.py --dataroot $DATAROOT --n_scenes 500 --n_img_each_scene 4 \
+    --checkpoints_dir 'checkpoints' --name 'room_chair' --results_dir 'results' \
+    --display_port $PORT --display_ncols 4 \
+    --load_size 128 --input_size 64 --render_size 8 --frustum_size 128 \
+    --n_samp 256 --num_slots 5 \
+    --model 'uorf_eval_T' \
+    --encoder_size 1024 \
+    --exp_id 'exp' \
+    --z_dim 48 --texture_dim 16 \
+    --project --attn_iter 4 \
+# done
+echo "Done"
